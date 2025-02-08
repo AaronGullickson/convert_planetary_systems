6524e121d150e1fc5df4ad1588e24f628c097fb5
Aaron Gullickson <aarongullickson@gmail.com>
2025-02-08 07:50:13 -0800

    Name xml lists by system id

--- convert_data.R
+++ convert_data.R
@@ -15,7 +15,28 @@ system_events <- read_xml("https://github.com/MegaMek/mekhq/raw/refs/heads/maste
 #system_name_change <- read_xml("https://github.com/MegaMek/mekhq/blob/master/MekHQ/data/universe/planetary_systems/system_namechanges.xml")
 system_connectors <- read_xml("https://github.com/MegaMek/mekhq/raw/refs/heads/master/MekHQ/data/universe/planetary_systems/system_connectors.xml")
 
+
+# Clean up input data -----------------------------------------------------
+
+# get children for all
+systems <- xml_children(systems)
+system_events <- xml_children(system_events)
+
+# name lists by id
+systems <- set_names(systems, 
+                     map_vec(systems, function(x) {
+                       return(xml_text(xml_find_first(x, "id")))
+                     }))
+system_events <- set_names(system_events, 
+                           map_vec(system_events, function(x) {
+                             return(xml_text(xml_find_first(x, "id")))
+                           }))
+
 # Test Case ---------------------------------------------------------------
+
+id <- "A Place"
 
 system_xml <- xml_child(systems, 1)
 psystem <- read_planetary_system(system_xml)
+
+cat(as.yaml(psystem, indent.mapping.sequence = TRUE, precision = 6))
